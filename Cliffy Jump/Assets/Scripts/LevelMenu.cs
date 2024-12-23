6c8b2e7433bc1f4b8c0bd22d7f8e21ee7b380693
DesdenLK <luca.acosta@estudiantat.upc.edu>
2024-12-23 21:35:11 +0100

    Percentages saved

--- Cliffy Jump/Assets/Scripts/LevelMenu.cs
+++ Cliffy Jump/Assets/Scripts/LevelMenu.cs
@@ -6,10 +6,22 @@ public class LevelMenu : MonoBehaviour
     public Dropdown levelDropdown;
     public Image levelImage;
     public Sprite[] levelSprite;
+    public Text percentageText;
+
+    private float[] percentages = { 0.0f, 0.0f };
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        if (!PlayerPrefs.HasKey("percentages"))
+        {
+            SaveArrayToPlayerPrefs();
+        }
+        else
+        {
+            LoadArrayFromPlayerPrefs();
+        }
+
         checkSelectedOption();
     }
 
@@ -19,6 +31,17 @@ public class LevelMenu : MonoBehaviour
         //UnityEngine.SceneManagement.SceneManager.LoadScene("Level" + levelDropdown.value);
         if (levelDropdown.value == 0) UnityEngine.SceneManagement.SceneManager.LoadScene("LucaProves");
         else UnityEngine.SceneManagement.SceneManager.LoadScene("AlbertProves");
+        PlayerPrefs.SetInt("level", levelDropdown.value);
+    }
+
+    public void ResetPercentages()
+    {
+        for (int i = 0; i < percentages.Length; i++)
+        {
+            percentages[i] = 0.0f;
+        }
+        SaveArrayToPlayerPrefs();
+        checkSelectedOption();
     }
 
 
@@ -27,5 +50,40 @@ public class LevelMenu : MonoBehaviour
         int selectedOption = levelDropdown.value;
         levelImage.sprite = levelSprite[selectedOption];
         levelDropdown.value = selectedOption;
+
+        percentageText.text = "Percentage: " + percentages[selectedOption] + "%";
+
     }
+
+    private void SaveArrayToPlayerPrefs()
+    {
+        string arrayString = string.Join("/", System.Array.ConvertAll(percentages, x => x.ToString("F1", System.Globalization.CultureInfo.InvariantCulture)));
+        PlayerPrefs.SetString("percentages", arrayString);
+        PlayerPrefs.Save();
+    }
+
+    private void LoadArrayFromPlayerPrefs()
+    {
+        if (PlayerPrefs.HasKey("percentages"))
+        {
+            string arrayString = PlayerPrefs.GetString("percentages");
+            Debug.Log("Loaded percentages: " + arrayString);
+
+            string[] stringArray = arrayString.Split('/');
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+
+                if (float.TryParse(stringArray[i], System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out float parsedValue))
+                {
+                    percentages[i] = parsedValue;
+                }
+                else
+                {
+                    Debug.LogWarning("Invalid percentage value found: " + stringArray[i]);
+                    percentages[i] = 0.0f; 
+                }
+            }
+        }
+    }
+
 }
--- Cliffy Jump/Assets/Scripts/ObjectCollision.cs
+++ Cliffy Jump/Assets/Scripts/ObjectCollision.cs
@@ -4,17 +4,27 @@ public class ObjectCollision : MonoBehaviour
 {
     public GameObject SceneManager;
     private SceneControl SceneControl;
+    private Distance_Percentage Distance_Percentage;
+
 
     private void Start()
     {
         SceneControl = SceneManager.GetComponent<SceneControl>();
+        Distance_Percentage = SceneManager.GetComponent<Distance_Percentage>();
     }
     private void OnCollisionEnter(Collision collision)
     {
         // Verifica si el objeto tiene el tag "Object"
         if (collision.gameObject.CompareTag("Object"))
         {
+            float percentage = Distance_Percentage.getPercentage();
+            Debug.Log("Percentage: " + percentage);
+            if (percentage > SceneControl.getPercentage())
+            {
+                SceneControl.setPercentage(percentage);
+            }
             SceneControl.Reset();
         }
     }
+
 }
