641d2881ecc6e181a6e5d3c794f288803509344c
DesdenLK <luca.acosta@estudiantat.upc.edu>
2025-01-01 17:49:42 +0100

    Added continue buttons to credits

---
+++ Cliffy Jump/Assets/Scripts/Credits.cs
@@ -0,0 +1,10 @@
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Credits : MonoBehaviour
+{
+    public void loadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Menu");
+    }
+}
